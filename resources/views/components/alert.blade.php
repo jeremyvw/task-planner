9959c0763e88b30135a20c6d218c6fd6055abd29
jeremyvw <jeremyvw30@gmail.com>
2020-06-14 03:26:37 +0700

    Change bootstrap to tailwind css

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,11 +2,11 @@
     <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
     {{$slot}}
     @if(session()->has('message'))
-        <div class="alert alert-success">
+        <div class="py-4 px-2 bg-green-300">
             {{session()->get('message')}}
         </div>
     @elseif(session()->has('error'))
-        <div class="alert alert-danger">
+        <div class="py-4 px-2 bg-red-300">
             {{session()->get('error')}}
         </div>
     @endif
