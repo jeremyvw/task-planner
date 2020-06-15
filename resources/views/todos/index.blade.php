22c9a5c1cea2e300fd824828bfb54b931665b9de
jeremyvw <jeremyvw30@gmail.com>
2020-06-15 17:58:49 +0700

    Add jump link

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,11 +1,15 @@
 @extends('todos.layout')
 
 @section('content')
-    <h1 class="text-2xl">All your To-Do's</h1>
-    <ul>
+    <div class="flex justify-center">
+        <h1 class="text-2xl">All your To-Do's</h1>
+        <a class="mx-5 py-1 px-1 bg-blue-400 cursor-pointer rounded text-white" href="/todos/create">Create New</a>
+    </div>
+    <ul class="my-5">
         @foreach ($todos as $todo)
-        <li>
-            {{$todo->title}}
+        <li class="flex justify-center py-2">
+            <p>{{$todo->title}}</p>
+            <a href="{{'/todos/'.$todo->id.'/edit'}}" class="mx-5 py-1 px-1 bg-orange-400 cursor-pointer rounded text-white">Edit</a>
         </li>
         @endforeach
     </ul>
