d1cd31991c97fc7a146eadc4343fbcc86d181edc
jeremyvw <jeremyvw30@gmail.com>
2020-06-17 21:10:14 +0700

    Add edit todo view

--- resources/views/todos/edit.blade.php
+++ resources/views/todos/edit.blade.php
@@ -1,5 +1,15 @@
 @extends('todos.layout')
 
 @section('content')
-    {{$todo->title}}    
+    <h1 class="text-2xl">Update this todo list</h1>
+    <h2>{{$todo->title}}</h2>    
+    <x-alert />
+    <form method="POST" action="/todos/create" class="py-5">
+        @csrf
+        <input type="text" name="title" class="py-2 px-2 border rounded" />
+        <input type="submit" value="Create" class="p-2 border rounded" />
+    </form>
+
+    <a href="/todos" class="m-5 py-1 px-1 bg-white-400 border cursor-pointer rounded ">Back</a>
+
 @endsection
