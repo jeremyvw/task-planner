2458e77a83774f5531cf862a648dc3f825ffeeb9
jeremyvw <jeremyvw30@gmail.com>
2020-06-18 20:49:10 +0700

    Change to named route

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex justify-between border-b pb-4 px-4">
         <h1 class="text-2xl">All your To-Do's</h1>
-        <a class="mx-5 py-2 text-blue-400 cursor-pointer text-white" href="/todos/create"><span class="fas fa-plus-circle"  /></a>
+        <a class="mx-5 py-2 text-blue-400 cursor-pointer text-white" href="{{route('todo.create')}}"><span class="fas fa-plus-circle"  /></a>
     </div>
     <ul class="my-5">
         <x-alert />
@@ -19,7 +19,7 @@
                 <p>{{$todo->title}}</p>
             @endif
             <div>
-                <a href="{{'/todos/'.$todo->id.'/edit'}}" class="text-orange-400 cursor-pointer text-white"><span class="fas fa-edit px-2" /></a>
+                <a href="{{route('todo.edit',$todo->id)}}" class="text-orange-400 cursor-pointer text-white"><span class="fas fa-edit px-2" /></a>
                 
                 <span onclick="event.preventDefault();
                                 if(confirm('Do you really want to delete this task?')){
@@ -27,7 +27,7 @@
                                     .submit()
                                 }"
                                 class="fas fa-trash text-red-500 px-2 cursor-pointer" />
-                <form style="display:none" id="{{'form-delete-'.$todo->id}}" method="POST" action="{{route('todo.delete', $todo->id)}}">
+                <form style="display:none" id="{{'form-delete-'.$todo->id}}" method="POST" action="{{route('todo.destroy', $todo->id)}}">
                     @csrf
                     @method('delete')    
                 </form>
