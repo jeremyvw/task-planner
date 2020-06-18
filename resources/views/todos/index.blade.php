6d66398dbde7a1b461a149fd96696fb1b4b85654
jeremyvw <jeremyvw30@gmail.com>
2020-06-18 16:13:55 +0700

    Add javascript to complete task

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -19,7 +19,14 @@
                 @if($todo->completed)
                     <span class="fas fa-check text-green-400 px-2" />
                 @else
-                    <span class="fas fa-check text-gray-300 cursor-pointer px-2" />
+                    <span onclick="event.preventDefault();
+                                    document.getElementById('form-complete-{{$todo->id}}')
+                                    .submit()" 
+                                    class="fas fa-check text-gray-300 cursor-pointer px-2" />
+                    <form style="display:none" id="{{'form-complete-'.$todo->id}}" method="POST" action="{{route('todo.complete', $todo->id)}}">
+                    @csrf
+                    @method('put')    
+                    </form>
                 @endif
             </div>
         </li>
