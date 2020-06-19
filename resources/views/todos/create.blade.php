ae125b6e8adb68d2358419e32be7a3efc2db6512
jeremyvw <jeremyvw30@gmail.com>
2020-06-19 15:43:10 +0700

    Add description field in create todo view

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -9,8 +9,15 @@
     <x-alert />
     <form method="POST" action="{{route('todo.store')}}" class="py-5">
         @csrf
-        <input type="text" name="title" class="py-2 px-2 border rounded" />
-        <input type="submit" value="Create" class="p-2 border rounded" />
+        <div class="py-1">
+            <input type="text" name="title" class="py-2 px-2 border rounded" placeholder="Title"/>
+        </div>
+        <div class="py-1">
+            <textarea name="description" cols="23" class="p-2 rounded border" placeholder="Description"></textarea>
+        </div>
+        <div class="py-1">
+            <input type="submit" value="Create" class="p-2 border rounded" />
+        </div>
     </form>
 
     <a href="{{route('todo.index')}}" class="m-5 py-1 px-1 bg-white-400 border cursor-pointer rounded ">Back</a>
