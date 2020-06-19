414b597449b3c646ca7efd37a8adfe2ab3e41aa1
jeremyvw <jeremyvw30@gmail.com>
2020-06-19 16:06:50 +0700

    Add todo detail view

---
+++ resources/views/todos/show.blade.php
@@ -0,0 +1,14 @@
+@extends('todos.layout')
+
+@section('content')
+    <div class="flex justify-between border-b pb-4 px-4">
+        <h1 class="text-2xl pb-4">{{$todo->title}}</h1>
+        <a class="mx-5 py-2 text-gray-400 cursor-pointer text-white" href="{{route('todo.index')}}"><span class="fas fa-arrow-left"  /></a>
+    </div>
+    <div>
+        <div>
+            <p>{{$todo->description}}</p>
+        </div>
+    </div>
+    
+@endsection
