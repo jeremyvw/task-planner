83bfb74cbe2c664d4c64cebd0eb60d186a6b6ef8
jeremyvw <jeremyvw30@gmail.com>
2020-06-21 00:20:53 +0700

    Fixed remove step field

--- resources/views/livewire/step.blade.php
+++ resources/views/livewire/step.blade.php
@@ -5,8 +5,8 @@
         <span wire:click="increment" class="fas fa-plus px-2 py-1 cursor-pointer" />
     </div>
     @foreach($steps as $step)
-        <div class="flex justify-center py-2">
-            <input type="text" name="step[]" class="py-1 px-2 border rounded" placeholder="{{'Describe step '.$step}}">
+        <div class="flex justify-center py-2" wire:key={{$step}}>
+            <input type="text" name="step[]" class="py-1 px-2 border rounded" placeholder="{{'Describe step '.($step+1)}}">
             <span class="fas fa-times text-red-400 p-2 cursor-pointer" wire:click="remove({{$loop->index}})" />
         </div>
     @endforeach
