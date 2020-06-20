ae69ed1707114f8b8c0cf4ed32cfc1b3e252c1dd
jeremyvw <jeremyvw30@gmail.com>
2020-06-20 18:50:14 +0700

    Add remove steps

--- resources/views/livewire/step.blade.php
+++ resources/views/livewire/step.blade.php
@@ -4,10 +4,10 @@
         <h2 class="text-lg pb-4">Add steps for task</h2>
         <span wire:click="increment" class="fas fa-plus px-2 py-1 cursor-pointer" />
     </div>
-    <div class="flex justify-center py-2">
-        @for ($i = 0; $i < $steps; $i++)
-            <input type="text" name="step" class="py-1 px-2 border rounded" placeholder="{{'Describe step '.($i+1)}}">
-            <span class="fas fa-times text-red-400 p-2" />
-        @endfor
-    </div>
+    @foreach($steps as $step)
+        <div class="flex justify-center py-2">
+            <input type="text" name="step[]" class="py-1 px-2 border rounded" placeholder="{{'Describe step '.$step}}">
+            <span class="fas fa-times text-red-400 p-2 cursor-pointer" wire:click="remove({{$loop->index}})" />
+        </div>
+    @endforeach
 </div>
