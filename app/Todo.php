0cf232bfea60b3d38facc803e0aa11e90f740485
jeremyvw <jeremyvw30@gmail.com>
2020-06-20 23:23:50 +0700

    Add hasMany relation to steps in todo model

--- app/Todo.php
+++ app/Todo.php
@@ -4,10 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Step;
+
 class Todo extends Model
 {
     protected $fillable = ['title', 'user_id', 'completed', 'description'];
     
+    public function steps()
+    {
+        return $this->hasMany(Step::class);
+    }
 
     // public function getRouteKeyName()
     // {
